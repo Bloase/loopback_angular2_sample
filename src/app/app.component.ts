a474cf42e1940e06daecce3fd7ccd9fce2bf5a4a
joaozitopolo <joaozito.polo@gmail.com>
2017-02-18 00:46:34 -0200

    update file management

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,7 @@
-import { Component, ElementRef } from '@angular/core';
-
-declare var $ : any;
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  templateUrl: 'app/app.component.html',
-  styleUrls: [ 'app/app.component.css' ]
+  template: `<h1>Hello {{name}}</h1>`,
 })
-export class AppComponent  { 
-  title = 'Angular'; 
-
-  constructor(el: ElementRef) {
-    setTimeout(() => {
-      $(el.nativeElement).foundation();
-    });
-  }
-
-}
+export class AppComponent  { name = 'Angular'; }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule }   from '@angular/router';
 
 import { AppComponent }  from './app.component';
-import { DashboardComponent }  from './dashboard.component';
 
 @NgModule({
-  imports:      [ BrowserModule,
-    RouterModule.forRoot([
-      {
-        path: '',
-        component: DashboardComponent
-      }
-    ])  
-  
-   ],
-  declarations: [ AppComponent, DashboardComponent ],
+  imports:      [ BrowserModule ],
+  declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
